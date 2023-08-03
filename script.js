b25fb6e649be4853976448e8dc150f34a38d7dc9
Mutisostud02 <mutiso8395@gmail.com>
2023-08-03 11:09:20 +0300

    Added functionality to display when game is a tie. Edited display to display result under the gameboard instead of top screen alert.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let playerSymbol = 'X';
 let gameOver = false;
+let results;
+
 const Board = (function () {
     const array = ['','','','','','','','',''];
     array.length = 9;
@@ -36,6 +38,10 @@ const displayController = (function () {
     const gameBoardDisp = document.createElement('div');
     gameBoardDisp.setAttribute('style', 'margin-left:30vw; display:grid; grid-template-rows: repeat(3, 80px); grid-template-columns: repeat(3, 80px); gap:2px;');
     body.appendChild(gameBoardDisp);
+    results = document.createElement('div');
+    results.setAttribute('id','9');
+    results.setAttribute('style', 'margin-top:3rem; font-size: 2rem; font-weight: 900; color: #22c56f; font-family: helvetica;')
+    body.appendChild(results);
     for (let i = 0; i < gameBoard.length; i++) {
         const boardDivs = document.createElement('div');
         boardDivs.setAttribute('id', `${i}`);
@@ -60,9 +66,10 @@ const displayController = (function () {
 
             boardDivs.removeEventListener('click',display)
             gameFlow();   
-            if (playerSymbol == 'X'){
+            gameDrawn();
+            if (playerSymbol == 'X' && !gameOver){
                 playerSymbol = Players.player2();
-            } else {
+            } else if (!gameOver) {
                 playerSymbol = Players.player1();
             }
             console.log('hey')           
@@ -76,6 +83,12 @@ const winPos = [
     [3, 4, 5], [1, 4, 7], [2, 4, 6],
     [6, 7, 8], [2, 5, 8]
     ];
+
+const drawPos = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+
+
+
 const gameFlow = function () {
     for (let j = 0; j < winPos.length; j++) {
         if (
@@ -83,15 +96,41 @@ const gameFlow = function () {
             document.getElementById(winPos[j][1]).innerHTML === playerSymbol && 
             document.getElementById(winPos[j][2]).innerHTML === playerSymbol) {
             document.getElementById(winPos[j][0]).setAttribute('style', 'color:white; background-color: #22c55e; font-size: 1.7rem; padding-top: 1.2rem; transform: scale(1.05);');
-            document.getElementById(winPos[j][1]).setAttribute('style', 'color:white; background-color: #22c55e; font-size: 1.7rem; padding-top: 1.2rem; transform: scale(1.05);')
-            document.getElementById(winPos[j][2]).setAttribute('style', 'color:white; background-color: #22c55e; font-size: 1.7rem; padding-top: 1.2rem; transform: scale(1.05);')
+            document.getElementById(winPos[j][1]).setAttribute('style', 'color:white; background-color: #22c55e; font-size: 1.7rem; padding-top: 1.2rem; transform: scale(1.05);');
+            document.getElementById(winPos[j][2]).setAttribute('style', 'color:white; background-color: #22c55e; font-size: 1.7rem; padding-top: 1.2rem; transform: scale(1.05);');
+            if (playerSymbol === 'X') {
+                playerSymbol = 'X';
+            } else if (playerSymbol === 'O'){
+                playerSymbol = 'O';
+            }
             gameOver = true;
             setTimeout(function() {
-                alert(playerSymbol + " wins!");
+
+                document.getElementById('9').textContent = 'GAME OVER!!! ' + playerSymbol + ' WINS';
                 }, 500);
                 }
-            }
+
         }
-
-
-
+    }
+ const gameDrawn = function () {
+                    if(
+            document.getElementById(drawPos[0]).innerHTML !== '' &&
+            document.getElementById(drawPos[1]).innerHTML !== '' &&
+            document.getElementById(drawPos[2]).innerHTML !== '' &&
+            document.getElementById(drawPos[3]).innerHTML !== '' &&
+            document.getElementById(drawPos[4]).innerHTML !== '' &&
+            document.getElementById(drawPos[5]).innerHTML !== '' &&
+            document.getElementById(drawPos[6]).innerHTML !== '' &&
+            document.getElementById(drawPos[7]).innerHTML !== '' &&
+            document.getElementById(drawPos[8]).innerHTML !== '' &&
+            !gameOver
+                )
+            {
+                gameOver = true;
+                setTimeout(() => {
+                    document.getElementById('9').setAttribute('style', 'color: red; margin-top:3rem; font-size: 2rem; font-weight: 900; font-family: helvetica;');
+                    document.getElementById('9').textContent = "GAME OVER!!! IT'S A TIE!";;
+                }, 300);
+                console.log(document.getElementById(drawPos[3].innerHTML))
+            }
+            }
